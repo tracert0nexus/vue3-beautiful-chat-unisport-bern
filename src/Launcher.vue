a1a4c6f57f85a505e8c67241425d583d5ab6be73
Jonathan <jschlede@pm.me>
2025-09-10 12:55:38 +0200

    gl-demo

--- src/Launcher.vue
+++ src/Launcher.vue
@@ -10,8 +10,8 @@
       <div v-if="newMessagesCount > 0 && !isOpen" class="sc-new-messsages-count">
         {{ newMessagesCount }}
       </div> <!-- TODO: Checkup before GoLive / PROD -->
-      <!--<img v-if="isOpen" class="sc-closed-icon" :src="icons.close.img" :alt="icons.close.name" />
-      <img v-else class="sc-open-icon" :src="icons.open.img" :alt="icons.open.name" /> -->
+      <img v-if="isOpen" class="sc-closed-icon" :src="icons.close.img" :alt="icons.close.name" />
+      <img v-else class="sc-open-icon" :src="icons.open.img" :alt="icons.open.name" />
     </div>
     <ChatWindow
       :message-list="messageList"
